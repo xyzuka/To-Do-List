c83afb8179bea3ea2bceb70c06c1df11dbf28cf1
xyzuka <yeebrandon@outlook.com>
2022-05-27 16:00:29 +1000

    HTML and CSS set up
    Webpack setup w HTML and CSS init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To Do List',
+      filename: 'index.html',
+      template: 'src/template.html',
+      favicon: 'src/Images/favicon.png',
+    }),
+  ],
+};
