7708315385d653d86faf025f2fcc1b102f56d265
xyzuka <yeebrandon@outlook.com>
2022-06-01 15:11:53 +1000

    Add to do list modal window

--- src/modules/modalWindows.js
+++ src/modules/modalWindows.js
@@ -1,8 +1,12 @@
-const show_modal = document.querySelector('.set-goal-prompt');
-const modal = document.querySelector('.modal');
+const show_goal_modal = document.querySelector('.set-goal-prompt');
+const show_to_do_modal = document.querySelector('.add-to-do-button');
+const modal = document.querySelector('.add-goal-modal');
+const to_do_modal = document.querySelector('.add-to-do-modal');
 const close_modal = document.querySelector('.close-modal');
+const close_to_do_modal = document.querySelector('.close-to-do-button');
 const overlay = document.querySelector('.overlay');
 const add_goal_btn = document.querySelector('.submit-goal');
+const add_to_do_btn = document.querySelector('.submit-to-do');
 
 export default function modalFeature() {
   const openModal = function () {
@@ -10,29 +14,40 @@ export default function modalFeature() {
     overlay.classList.remove('hide');
   };
 
-  show_modal.addEventListener('click', openModal);
+  const openToDoModal = function () {
+    to_do_modal.classList.remove('hide');
+    overlay.classList.remove('hide');
+  };
 
-  const closeModal = function () {
+  show_goal_modal.addEventListener('click', openModal);
+
+  show_to_do_modal.addEventListener('click', openToDoModal);
+
+  const closeModals = function () {
     modal.classList.add('hide');
+    to_do_modal.classList.add('hide');
     overlay.classList.add('hide');
   };
 
-  close_modal.addEventListener('click', closeModal);
+  close_modal.addEventListener('click', closeModals);
 
-  overlay.addEventListener('click', closeModal);
+  close_to_do_modal.addEventListener('click', closeModals);
+
+  overlay.addEventListener('click', closeModals);
 
   document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape') {
-      closeModal();
+      closeModals();
     }
   });
 
-  // Need to write function to close and also send the data to database
-  add_goal_btn.addEventListener('click', closeModal);
+  add_goal_btn.addEventListener('click', closeModals);
+
+  add_to_do_btn.addEventListener('click', closeModals);
 
   document.addEventListener('keydown', function (e) {
     if (e.key === 'Enter') {
-      closeModal();
+      closeModals();
     }
   });
 }
